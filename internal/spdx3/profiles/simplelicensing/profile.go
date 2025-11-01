582d4978f2a169c21950647c1d37357faa446a1d
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2025-11-01 13:03:49 -0600

    Add missing types to simple licensing
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

--- internal/spdx3/profiles/simplelicensing/profile.go
+++ internal/spdx3/profiles/simplelicensing/profile.go
@@ -7,13 +7,33 @@ import (
 
 const Prefix = "simplelicensing "
 
-// simplelicensing_LicenseExpression
-type LicenseExpression struct {
+// AnyLicenseInfo is the abstract base class for license information
+type AnyLicenseInfo struct {
 	core.Node
-	LicenseExpression  string `json:"simplelicensing_licenseExpression"`
-	LicenseListVersion string `json:"simplelicensing_licenseListVersion"`
+}
+
+func (ali *AnyLicenseInfo) UnmarshalJSON(data []byte) error {
+	return unmarshal.Node(data, ali, &ali.PreNode)
+}
+
+// LicenseExpression represents licenses expressed as license expression strings
+type LicenseExpression struct {
+	AnyLicenseInfo
+	CustomIdToUri      []core.DictionaryEntry `json:"customIdToUri,omitempty"`
+	LicenseExpression  string                 `json:"simplelicensing_licenseExpression"`
+	LicenseListVersion string                 `json:"simplelicensing_licenseListVersion,omitempty"`
 }
 
 func (le *LicenseExpression) UnmarshalJSON(data []byte) error {
 	return unmarshal.Node(data, le, &le.PreNode)
+}
+
+// SimpleLicensingText represents license text not on the SPDX License List
+type SimpleLicensingText struct {
+	core.Node
+	LicenseText string `json:"licenseText"`
+}
+
+func (slt *SimpleLicensingText) UnmarshalJSON(data []byte) error {
+	return unmarshal.Node(data, slt, &slt.PreNode)
 }
