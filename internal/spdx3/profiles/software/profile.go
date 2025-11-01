8b35ef4dfab13250a093fd242970d5dac8e726a3
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2025-11-01 11:20:09 -0600

    Add software package type
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

--- internal/spdx3/profiles/software/profile.go
+++ internal/spdx3/profiles/software/profile.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/carabiner-dev/databom/internal/spdx3/profiles/core"
+	"github.com/carabiner-dev/databom/internal/spdx3/types"
 	"github.com/carabiner-dev/databom/internal/spdx3/unmarshal"
 )
 
@@ -18,26 +19,59 @@ type Node struct {
 // File
 type File struct {
 	Node
-	AdditionalPurpose  []string     `json:"additionalPurpose,omitempty"`
-	AttributionText    []string     `json:"attributionText,omitempty"`
-	BuiltTime          *time.Time   `json:"builtTime,omitempty"`
-	ContentIdentifier  []string     `json:"contentIdentifier,omitempty"`
-	ContentType        string       `json:"contentType,omitempty"`
-	CopyrightText      string       `json:"copyrightText,omitempty"`
-	Extension          []string     `json:"extension,omitempty"`
-	ExternalIdentifier []string     `json:"externalIdentifier,omitempty"`
-	ExternalRef        []string     `json:"externalRef,omitempty"`
-	FileKind           string       `json:"fileKind,omitempty"`
-	OriginatedBy       []string     `json:"originatedBy,omitempty"`
-	ReleaseTime        *time.Time   `json:"releaseTime,omitempty"`
-	StandardName       []string     `json:"standardName,omitempty"`
-	Summary            string       `json:"summary,omitempty"`
-	SuppliedBy         string       `json:"suppliedBy,omitempty"`
-	SupportLevel       []string     `json:"supportLevel,omitempty"`
-	ValidUntilTime     *time.Time   `json:"validUntilTime,omitempty"`
-	VerifiedUsing      []string     `json:"verifiedUsing,omitempty"`
+	AdditionalPurpose  []string      `json:"additionalPurpose,omitempty"`
+	AttributionText    []string      `json:"attributionText,omitempty"`
+	BuiltTime          *time.Time    `json:"builtTime,omitempty"`
+	ContentIdentifier  []string      `json:"contentIdentifier,omitempty"`
+	ContentType        string        `json:"contentType,omitempty"`
+	CopyrightText      string        `json:"copyrightText,omitempty"`
+	Extension          []string      `json:"extension,omitempty"`
+	ExternalIdentifier []string      `json:"externalIdentifier,omitempty"`
+	ExternalRef        []string      `json:"externalRef,omitempty"`
+	FileKind           string        `json:"fileKind,omitempty"`
+	OriginatedBy       []*core.Agent `json:"originatedBy,omitempty"`
+	ReleaseTime        *time.Time    `json:"releaseTime,omitempty"`
+	StandardName       []string      `json:"standardName,omitempty"`
+	Summary            string        `json:"summary,omitempty"`
+	SuppliedBy         *core.Agent   `json:"suppliedBy,omitempty"`
+	SupportLevel       []string      `json:"supportLevel,omitempty"`
+	ValidUntilTime     *time.Time    `json:"validUntilTime,omitempty"`
+	VerifiedUsing      []string      `json:"verifiedUsing,omitempty"`
 }
 
 func (f *File) UnmarshalJSON(data []byte) error {
 	return unmarshal.Node(data, f, &f.PreNode)
+}
+
+type IntegrityMethod interface {
+	GetComment() string
+}
+
+// Package
+type Package struct {
+	Node
+	AdditionalPurpose  []string          `json:"additionalPurpose,omitempty"`
+	AttributionText    []string          `json:"attributionText,omitempty"`
+	BuiltTime          *time.Time        `json:"builtTime,omitempty"`
+	ContentIdentifier  []string          `json:"contentIdentifier,omitempty"`
+	CopyrightText      string            `json:"copyrightText,omitempty"`
+	Extension          []string          `json:"extension,omitempty"`
+	ExternalIdentifier []string          `json:"externalIdentifier,omitempty"`
+	ExternalRef        []string          `json:"externalRef,omitempty"`
+	HomePage           string            `json:"homePage,omitempty"`
+	OriginatedBy       []types.Node      `json:"originatedBy,omitempty"`
+	PackageUrl         string            `json:"packageUrl,omitempty"`
+	PackageVersion     string            `json:"packageVersion,omitempty"`
+	ReleaseTime        *time.Time        `json:"releaseTime,omitempty"`
+	SourceInfo         string            `json:"sourceInfo,omitempty"`
+	StandardName       []string          `json:"standardName,omitempty"`
+	Summary            string            `json:"summary,omitempty"`
+	SuppliedBy         *core.Agent       `json:"suppliedBy,omitempty"`
+	SupportLevel       []string          `json:"supportLevel,omitempty"`
+	ValidUntilTime     *time.Time        `json:"validUntilTime,omitempty"`
+	VerifiedUsing      []IntegrityMethod `json:"verifiedUsing,omitempty"`
+}
+
+func (p *Package) UnmarshalJSON(data []byte) error {
+	return unmarshal.Node(data, p, &p.PreNode)
 }
