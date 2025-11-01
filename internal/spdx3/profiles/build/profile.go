132075c4013a7f6f3553b415e981b2adb2b076c7
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2025-11-01 13:28:19 -0600

    Add build profile types
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

---
+++ internal/spdx3/profiles/build/profile.go
@@ -0,0 +1,28 @@
+package build
+
+import (
+	"time"
+
+	"github.com/carabiner-dev/databom/internal/spdx3/profiles/core"
+	"github.com/carabiner-dev/databom/internal/spdx3/unmarshal"
+)
+
+const Prefix = "build"
+
+// Build represents the act of converting software inputs into software artifacts
+type Build struct {
+	core.Node
+	BuildEndTime            *time.Time             `json:"buildEndTime,omitempty"`
+	BuildId                 string                 `json:"buildId,omitempty"`
+	BuildStartTime          *time.Time             `json:"buildStartTime,omitempty"`
+	BuildType               string                 `json:"buildType"`
+	ConfigSourceDigest      []core.Hash            `json:"configSourceDigest,omitempty"`
+	ConfigSourceEntrypoint  []string               `json:"configSourceEntrypoint,omitempty"`
+	ConfigSourceUri         []string               `json:"configSourceUri,omitempty"`
+	Environment             []core.DictionaryEntry `json:"environment,omitempty"`
+	Parameter               []core.DictionaryEntry `json:"parameter,omitempty"`
+}
+
+func (b *Build) UnmarshalJSON(data []byte) error {
+	return unmarshal.Node(data, b, &b.PreNode)
+}
